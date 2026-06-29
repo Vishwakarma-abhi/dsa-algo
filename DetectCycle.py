069dd033e0a1e04a8382ecc0100e2e40e6f540b2
Vishwakarm-abhi <vishwakarma2003@hotmail.com>
2026-06-29 23:30:01 +0530

    LinkedList

---
+++ DetectCycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # base case
+        if head == None:
+            return False  # --> No cycle
+
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
