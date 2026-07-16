6548cce6f57db987c7c078930b1ffdd623a2d3b3
Vishwakarm-abhi <vishwakarma2003@hotmail.com>
2026-07-16 23:51:00 +0530

    BackTracking

---
+++ subset_backtracking.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            # choice 1 ->  pick it
+            subset.append(nums[i])
+            # explore
+            dfs(i+1)
+            # choice 2 -> exclude it
+            subset.pop()
+            # explore without it
+            dfs(i+1)
+
+        
+        dfs(0)
+        return res
+
